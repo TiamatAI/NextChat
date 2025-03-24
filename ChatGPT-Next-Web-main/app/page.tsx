aa66c791064deb6434c54a8809a6de587457bd80
lmy <limingyu23333@163.com>
2025-03-24 17:37:44 +0800

    init

--- ChatGPT-Next-Web-main/app/page.tsx
+++ ChatGPT-Next-Web-main/app/page.tsx
@@ -7,6 +7,7 @@ import { getServerSideConfig } from "./config/server";
 const serverConfig = getServerSideConfig();
 
 export default async function App() {
+  console.log("aa");
   return (
     <>
       <Home />
